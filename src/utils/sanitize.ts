6584dd9895523335640a23fd3231ad284c46c8fb
Mark Tamis <mark.tamis@gmail.com>
2024-09-17 12:10:08 +0200

    fix: sanitize function

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -3,10 +3,10 @@ import type { IMessage } from '..'
 export function sanitize(messages: IMessage[]): IMessage[] {
   messages.forEach((message: IMessage) => {
     // remove all links
-    message.content = message.content.replace(/(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)((\?.*)?)/g, '') /* eslint-disable-line regexp/no-misleading-capturing-group */
+    message.content = message?.content.replace(/(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)((\?.*)?)/g, '') || '' /* eslint-disable-line regexp/no-misleading-capturing-group */
 
     if (message.sender !== null) {
-      if (message.sender?.title === 'Unknown author') {
+      if (message.sender?.title === 'Unknown author' || message.sender?.title === null) {
         message.sender.title = ''
       }
       if (message.sender?.name !== '' && message.sender.title === '') {
