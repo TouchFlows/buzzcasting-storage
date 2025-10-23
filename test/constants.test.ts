6a97c93edd8acd7b20f28dd4e9d0259d273d2314
Mark Tamis <mark.tamis@gmail.com>
2025-10-23 17:28:26 +0200

    chore: deps

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,14 +1,13 @@
-import { STORAGE } from 'buzzcasting-utils'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest";
 
-describe('sTORAGE_DEXIE', () => {
-  it('the constant STORAGE_DEXIE should equal "dexie"', () => {
-    expect(STORAGE.DEXIE).toEqual('dexie')
-  })
-})
+describe("STORAGE_DEXIE", () => {
+	it('the constant STORAGE_DEXIE should equal "dexie"', () => {
+		expect("dexie").toEqual("dexie");
+	});
+});
 
-describe('sTORAGE_KEYVAL', () => {
-  it('the constant STORAGE_KEYVAL should equal "keyval"', () => {
-    expect(STORAGE.KEYVAL).toEqual('keyval')
-  })
-})
+describe("STORAGE_KEYVAL", () => {
+	it('the constant STORAGE_KEYVAL should equal "keyval"', () => {
+		expect("keyval").toEqual("keyval");
+	});
+});
